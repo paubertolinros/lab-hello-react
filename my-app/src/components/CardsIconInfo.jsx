5de8cc535324593dbba34ca45f715381dffabaa6
Pau Bertolin <paulaberos@gmail.com>
2023-02-22 16:46:35 +0100

    change cards info text and change some CSS

--- my-app/src/components/CardsIconInfo.jsx
+++ my-app/src/components/CardsIconInfo.jsx
@@ -13,18 +13,18 @@ const CardsIconInfo = () => {
       </div> 
       <div className="card-icons-info-div">
         <img src={icon2} alt="Ironhack logo" />
-        <h3> Declarative </h3>
-        <p> React makes it painless to create interactive UIs. </p>
+        <h3> Components </h3>
+        <p> Build encapsulated components that manage their state. </p>
       </div>
       <div className="card-icons-info-div">  
         <img src={icon3} alt="Ironhack logo" />
-        <h3> Declarative </h3>
-        <p> React makes it painless to create interactive UIs. </p>
+        <h3> Single-Way </h3>
+        <p> A set of immutable values are passed to the component's. </p>
       </div>
       <div className="card-icons-info-div">
         <img src={icon4} alt="Ironhack logo" />
-        <h3> Declarative </h3>
-        <p> React makes it painless to create interactive UIs. </p>
+        <h3> JSX </h3>
+        <p> Statically-typed, designed to run on modern browsers. </p>
       </div>
     </>
   )
